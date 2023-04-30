6a670bdb0e501aa57105e128dfd59b8676ea9784
MuthukumarNK <dudekumar12@gmail.com>
2023-05-01 00:17:23 +0530

    update

--- src/main/java/com/wipro/velocity/empowerher/controller/SpaController.java
+++ src/main/java/com/wipro/velocity/empowerher/controller/SpaController.java
@@ -1,13 +1,42 @@
 package com.wipro.velocity.empowerher.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.WebUtils;
+
+@Controller
 public class SpaController {
 
-	
-	@RequestMapping(value = "/**/{path:[^\\.]*}")
-	public String redirectToIndex() {
-	    return "forward:/index.html";
-	}
+    @RequestMapping(value = "/**/{path:[^\\.]*}")
+    public String redirectToIndex(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (request.getRequestURI().startsWith("/api/")) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return null;
+        }
+        if (isIncludeStackTrace(request, MediaType.ALL)) {
+            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, new RuntimeException("Stack trace"));
+        }
+        return "forward:/index.html";
+    }
+
+    private boolean isIncludeStackTrace(HttpServletRequest request, MediaType produces) {
+        String parameter = request.getParameter("trace");
+        if (parameter == null) {
+            return false;
+        }
+        if (!"false".equalsIgnoreCase(parameter)) {
+            return true;
+        }
+        MediaType acceptableMediaType = MediaType.parseMediaTypes(request.getHeader("Accept")).get(0);
+        return acceptableMediaType.includes(produces);
+    }
 
 }
+
+
